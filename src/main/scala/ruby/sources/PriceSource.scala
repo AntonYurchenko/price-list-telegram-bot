7d196d81f749943bf0b075d4f3f2ff31e040e5cc
Anton Yurchenko <yurchenko@restream.rt.ru>
2017-08-22 17:18:24 +0300

    is-2: Function of writing of new price list and changing user status (client/admin) has been implemented

--- src/main/scala/ruby/sources/PriceSource.scala
+++ src/main/scala/ruby/sources/PriceSource.scala
@@ -1,6 +1,7 @@
 package ruby.sources
 
-import java.io.File
+import java.io.{File, FileWriter}
+import java.nio.file.{Files, StandardCopyOption}
 
 import scala.io.Source
 
@@ -10,11 +11,29 @@ object PriceSource {
     val fileList = dir.listFiles()
     if (fileList.isEmpty) Nil
     else {
-      val lastTsFile = fileList.max
+      val lastTsFile = fileList.filter(_.isFile).max
       val price = Source.fromFile(lastTsFile, "UTF-8").getLines().toSeq
       if (price.length <= 100) price :: Nil
       else price.grouped(100).toSeq
     }
+  }
+
+  def write(file: File, content: String, append: Boolean = true): Boolean = try {
+    val writer = new FileWriter(file, append)
+    writer.write(content)
+    writer.flush()
+    writer.close()
+    true
+  } catch {
+    case _: Exception => false
+  }
+
+
+  def move(file1: File, file2: File): Boolean = try {
+    Files.move(file1.toPath, file2.toPath, StandardCopyOption.ATOMIC_MOVE)
+    true
+  } catch {
+    case _: Exception => false
   }
 
 }
